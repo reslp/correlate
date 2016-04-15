a463a2adf8f731bf034f9e9b1c12a9809ea693ec
Philipp Resl <resl.philipp@gmail.com>
2016-04-15 15:21:02 +0200

    update

--- R/correlate.R
+++ R/correlate.R
@@ -1,3 +1,7 @@
+## correlate
+## written by Philipp Resl
+## last update April 15, 2016
+
 require(phytools)
 
 calculate_overlap <- function(c1_min=0, c1_max=0, c2_min=0, c2_max=0){
@@ -13,8 +17,8 @@ calculate_overlap <- function(c1_min=0, c1_max=0, c2_min=0, c2_max=0){
   return(0) #case 6
 }  
 
-correlate <- function(ntrees=1,nmaps=1,smap1=simmap, smap2=simmap_multi, chars2= c("wood", "bark", "rock", "soil", "lichen"), chars1 = c(0,1)) {
-### function currently only accepts lists of multiPhylo objects!!
+correlate <- function(ntrees=1,nmaps=1,smap1=simmap, smap2=simmap_multi, chars2= c(0,1), chars1 = c(0,1)) {
+### function currently only accepts lists of multiPhylo objects, stochastic maps of single trees will be converted to lists
 if (class(smap1) != "list") { #single trees, behaviour unclear for otherwise formatted phylo objects
     sim <- vector("list", 1)
     sim[[1]] <- smap1 
@@ -24,7 +28,8 @@ if (class(smap2) != "list") { #single trees, behaviour unclear for otherwise for
     simm <- vector("list", 1)
     simm[[1]] <- smap2 
 } else { simm <- smap2 }
-  number_rows <- length(sim[[1]][[1]]$edge)
+
+number_rows <- length(sim[[1]][[1]]$edge)
 ncols <- length(chars1)*length(chars2)
 df <- data.frame(matrix(0, ncol=ncols, nrow=ntrees))
 
@@ -50,9 +55,11 @@ for (i in 1:number_rows) {
   if ((length(char_1_states) == 1) && (length(char_2_states) == 1) && names(char_1_states)==char1 && names(char_2_states)==char2) { 
     ## only one state mapped on edge
     #in this case correlation is 100% of edge length
+    #print("Only one state")
     shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
   } else if ((length(char_1_states) != 1) && (length(char_2_states) == 1) && names(char_1_states)==char1 && names(char_2_states)==char2) {
       ## two states of char 1 mapped on edge
+      #print("Two states in char 1")
       char1_max <- 0
       for (j in 1:length(char_1_states)) {
         if (names(char_1_states[j]) == char1) {
@@ -61,6 +68,7 @@ for (i in 1:number_rows) {
       }
       shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
     } else if ((length(char_1_states) == 1) && (length(char_2_states) != 1) && names(char_1_states)==char1 && names(char_2_states)==char2) {
+      #print("Two states in char 2")  
       ## two states of char 2 mapped on edge
         char2_max <- 0
         for (j in 1:length(char_2_states)) {
@@ -71,6 +79,7 @@ for (i in 1:number_rows) {
         }
         shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
     } else if ((length(char_1_states) > 1) && (length(char_2_states) > 1)) {
+      #print("Both states >1")
         for (k in 1:length(char_1_states)) {
           if (names(char_1_states[k])==char1) {
             if (k == 1) {
@@ -86,12 +95,16 @@ for (i in 1:number_rows) {
                 char2_min <- 0
                 char2_max <- as.numeric(char_2_states[l])
                 shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
+                #cat("Total overlap:", simm[[tr]][[mp]]$mapped.edge[i], "\n")
+                #cat("calculated overlap:", shared_time_tree, "\n")
               } else {
                       char2_min <- as.numeric(sum(char_2_states[1:l-1]))
                       char2_max <- as.numeric(sum(char_2_states[1:l])) 
                       shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
                     } 
-              }
+              
+              #### here happens the important stuff          
+            }
             }
           }
         }
@@ -112,6 +125,11 @@ return(df)
 #function get probability, will extract P(character) from the provided stochastic maps per tree
 #needed for later calculation of conditional
 get_prob <- function(sm){
+  if (class(sm) != "list") { #single trees, behaviour unclear for otherwise formatted phylo objects
+    sim <- vector("list", 1)
+    sim[[1]] <- sm
+    sm <- sim
+  }
   ntrees <- length(sm)
   nmaps <- length(sm[[1]])
   time_matrix <- vector()
@@ -123,8 +141,13 @@ get_prob <- function(sm){
   for (c in characters) {
     whichchar <- whichchar + 1
     colnames(df)[whichchar] <- c
+    #print("char")
+    #print(c)
     for (i in 1:ntrees) {
+      #print("i")
+      #print(i)
       edge_length <- sum(sm[[i]][[1]]$edge.length)
+      #print(sum(edge_length))
       time_matrix <- vector()
       for (j in 1:nmaps) {
         time_matrix  <- c(time_matrix,sum(as.numeric(sm[[i]][[j]]$mapped.edge[,whichchar]))) #1=character 0, 2=character 1,...
@@ -151,21 +174,25 @@ get_conditional <- function(matrix, probs) {
   for (i in 1:ncol(matrix)){
     print(i)
     if (iseven(i) == TRUE) {
+      #print("even")
+      #print(i)
       matrix[i] <- matrix[i]/probs[which]
       which <- which + 1
     }
     else { 
+      #print("else")
       matrix[i] <- matrix[i]/probs[which]
       }
   }
   return(matrix)
 }
 
-plot_correlation <- function(cond=cond, title="") {
+plot_correlation <- function(cond=cond, title="", xlab="", ylab="") {
   require(reshape2)
   require(ggplot2)
   df <- melt(cond)
-  p <- ggplot(data = df, aes(x=variable, y=value))  + geom_violin(aes(fill=variable)) + stat_summary(fun.data=mean_sdl, mult=1, geom="pointrange", color="black") + labs(title = "title") +theme(plot.title = element_text(size = rel(0.9)))
-  p
+  p <- ggplot(data = df, aes(x=variable, y=value))  + geom_violin(aes(fill=variable)) + stat_summary(fun.data=mean_sdl, mult=1, geom="pointrange", color="black") + labs(title = title) +theme(plot.title = element_text(size = rel(0.9)))
+  if (xlab != "") {p <- p + xlab(xlab)}
+  if (ylab != "") {p <- p + ylab(ylab)}
   return(p)
 }
