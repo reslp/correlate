6448010d130dfdb935aea3e4c986aa97fc205fb3
Philipp Resl <resl.philipp@gmail.com>
2016-04-15 16:59:05 +0200

    summarize

--- R/correlate.R
+++ R/correlate.R
@@ -35,90 +35,110 @@ df <- data.frame(matrix(0, ncol=ncols, nrow=ntrees))
 
 whichmatrix <- 0
 whichchar <- 0
-for(chs2 in chars2) {
-char2 <- chs2
-for(chs1 in chars1) {
-char1 <- chs1
-whichchar <- whichchar + 1
-colnames(df)[whichchar] <- paste(char1,"_",char2,sep="") 
-for(tr in 1:ntrees) {
-shared_time_tree <- vector()
-for(mp in 1:nmaps) {
-cat("For ", chs2, " and ", chs1, " Working on Tree ", tr, " Map ", mp, "\n")
-for (i in 1:number_rows) { 
-  char1_min <- 0
-  char1_max <- sim[[tr]][[mp]]$edge.length[i]
-  char2_min <- 0
-  char2_max <- simm[[tr]][[mp]]$edge.length[i]  
-  char_1_states <- unlist(sim[[tr]][[mp]]$maps[i])
-  char_2_states <- unlist(simm[[tr]][[mp]]$maps[i])  
-  if ((length(char_1_states) == 1) && (length(char_2_states) == 1) && names(char_1_states)==char1 && names(char_2_states)==char2) { 
-    ## only one state mapped on edge
-    #in this case correlation is 100% of edge length
-    #print("Only one state")
-    shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
-  } else if ((length(char_1_states) != 1) && (length(char_2_states) == 1) && names(char_1_states)==char1 && names(char_2_states)==char2) {
-      ## two states of char 1 mapped on edge
-      #print("Two states in char 1")
-      char1_max <- 0
-      for (j in 1:length(char_1_states)) {
-        if (names(char_1_states[j]) == char1) {
-          char1_max <- char1_max + as.numeric(char_1_states[j])
-        } 
-      }
-      shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
-    } else if ((length(char_1_states) == 1) && (length(char_2_states) != 1) && names(char_1_states)==char1 && names(char_2_states)==char2) {
-      #print("Two states in char 2")  
-      ## two states of char 2 mapped on edge
-        char2_max <- 0
-        for (j in 1:length(char_2_states)) {
-          if (names(char_2_states[j]) == char2) {
-            char2_max <- char2_max + as.numeric(char_2_states[j])
-            
-          } 
-        }
-        shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
-    } else if ((length(char_1_states) > 1) && (length(char_2_states) > 1)) {
-      #print("Both states >1")
-        for (k in 1:length(char_1_states)) {
-          if (names(char_1_states[k])==char1) {
-            if (k == 1) {
-              char1_min <- 0
-              char1_max <- as.numeric(char_1_states[k])
-            } else {
-                char1_min <- as.numeric(sum(char_1_states[1:k-1]))
-                char1_max <- as.numeric(sum(char_1_states[1:k]))
-              }  
-          for (l in 1:length(char_2_states)) {
-            if(names(char_2_states[l])==char2) {
-              if (l == 1) {
-                char2_min <- 0
-                char2_max <- as.numeric(char_2_states[l])
-                shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
-                #cat("Total overlap:", simm[[tr]][[mp]]$mapped.edge[i], "\n")
-                #cat("calculated overlap:", shared_time_tree, "\n")
-              } else {
-                      char2_min <- as.numeric(sum(char_2_states[1:l-1]))
-                      char2_max <- as.numeric(sum(char_2_states[1:l])) 
-                      shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
-                    } 
-              
-              #### here happens the important stuff          
-            }
-            }
-          }
-        }
-      }   else { #shared probability on edge is 0
-                shared_time_tree <- c(shared_time_tree, 0)
-                }
-} #loop i
-} #loop mp
-
-df[tr,whichchar] <- (sum(shared_time_tree)/nmaps)/sum(sim[[tr]][[1]]$edge.length) #divided by number of maps and tree length
-} #loop tr
-} #loop chs1
-} #loop chs2
-return(df)
+for(chs2 in chars2) 
+  {
+    char2 <- chs2
+    for(chs1 in chars1) 
+      {
+        char1 <- chs1
+        whichchar <- whichchar + 1
+        colnames(df)[whichchar] <- paste(char1,"_",char2,sep="") 
+        for(tr in 1:ntrees) 
+          {
+            shared_time_tree <- vector()
+            for(mp in 1:nmaps) 
+              {
+                cat("For ", chs2, " and ", chs1, " Working on Tree ", tr, " Map ", mp, "\n")
+                for (i in 1:number_rows) 
+                  { 
+                    char1_min <- 0
+                    char1_max <- sim[[tr]][[mp]]$edge.length[i]
+                    char2_min <- 0
+                    char2_max <- simm[[tr]][[mp]]$edge.length[i]  
+                    char_1_states <- unlist(sim[[tr]][[mp]]$maps[i])
+                    char_2_states <- unlist(simm[[tr]][[mp]]$maps[i])  
+                    if ((length(char_1_states) == 1) && (length(char_2_states) == 1) && names(char_1_states)==char1 && names(char_2_states)==char2) 
+                      { 
+                        # only one state mapped on edge
+                        # in this case correlation is 100% of edge length
+                        shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
+                      } 
+                    else if ((length(char_1_states) != 1) && (length(char_2_states) == 1) && names(char_1_states)==char1 && names(char_2_states)==char2) 
+                      {
+                        # two states of char 1 mapped on edge
+                        char1_max <- 0
+                        for (j in 1:length(char_1_states)) 
+                          {
+                            if (names(char_1_states[j]) == char1) 
+                              {
+                                char1_max <- char1_max + as.numeric(char_1_states[j])
+                              } 
+                          }
+                        shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
+                      }
+                    else if ((length(char_1_states) == 1) && (length(char_2_states) != 1) && names(char_1_states)==char1 && names(char_2_states)==char2) 
+                      {
+                        #print("Two states in char 2")  
+                        ## two states of char 2 mapped on edge
+                        char2_max <- 0
+                        for (j in 1:length(char_2_states)) 
+                          {
+                            if (names(char_2_states[j]) == char2) 
+                              {
+                                char2_max <- char2_max + as.numeric(char_2_states[j])
+                              } 
+                          }
+                        shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
+                      } 
+                    else if ((length(char_1_states) > 1) && (length(char_2_states) > 1)) 
+                      {
+                        #print("Both states >1")
+                        for (k in 1:length(char_1_states)) 
+                          {
+                            if (names(char_1_states[k])==char1) 
+                              {
+                                if (k == 1) 
+                                  {
+                                    char1_min <- 0
+                                    char1_max <- as.numeric(char_1_states[k])
+                                  } 
+                                else 
+                                  {
+                                    char1_min <- as.numeric(sum(char_1_states[1:k-1]))
+                                    char1_max <- as.numeric(sum(char_1_states[1:k]))
+                                  }  
+                                for (l in 1:length(char_2_states)) 
+                                  {
+                                    if(names(char_2_states[l])==char2) 
+                                      {
+                                        if (l == 1) 
+                                          {
+                                            char2_min <- 0
+                                            char2_max <- as.numeric(char_2_states[l])
+                                            shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
+                                          } 
+                                        else 
+                                          {
+                                            char2_min <- as.numeric(sum(char_2_states[1:l-1]))
+                                            char2_max <- as.numeric(sum(char_2_states[1:l])) 
+                                            shared_time_tree <- c(shared_time_tree, calculate_overlap(char1_min, char1_max, char2_min, char2_max))
+                                          } 
+                                      }
+                                  }
+                              }
+                          }
+                      }   
+                      else 
+                        { #shared probability on edge is 0
+                          shared_time_tree <- c(shared_time_tree, 0)
+                        }
+                  } #loop i
+              } #loop mp
+              df[tr,whichchar] <- (sum(shared_time_tree)/nmaps)/sum(sim[[tr]][[1]]$edge.length) #divided by number of maps and tree length
+          } #loop tr
+      } #loop chs1
+  } #loop chs2
+  return(df)
 } #function calc_overlap
 # calculate relative times
 
@@ -141,13 +161,8 @@ get_prob <- function(sm){
   for (c in characters) {
     whichchar <- whichchar + 1
     colnames(df)[whichchar] <- c
-    #print("char")
-    #print(c)
     for (i in 1:ntrees) {
-      #print("i")
-      #print(i)
       edge_length <- sum(sm[[i]][[1]]$edge.length)
-      #print(sum(edge_length))
       time_matrix <- vector()
       for (j in 1:nmaps) {
         time_matrix  <- c(time_matrix,sum(as.numeric(sm[[i]][[j]]$mapped.edge[,whichchar]))) #1=character 0, 2=character 1,...
@@ -166,21 +181,15 @@ iseven <- function(x) x %% 2 == 0
 
 ## calculates conditional from dfs created with correlate() and get_prob()
 get_conditional <- function(matrix, probs) {
-  #number <- ncol(matrix) / ncol(probs)
-  #probs_expanded <- do.call("cbind", replicate(number, probs, simplify=F))
-  #conditional <- matrix / probs_expanded
   increment <- ncol(matrix) / ncol(probs)
   which <- 1
   for (i in 1:ncol(matrix)){
     print(i)
     if (iseven(i) == TRUE) {
-      #print("even")
-      #print(i)
       matrix[i] <- matrix[i]/probs[which]
       which <- which + 1
     }
     else { 
-      #print("else")
       matrix[i] <- matrix[i]/probs[which]
       }
   }
@@ -196,3 +205,20 @@ plot_correlation <- function(cond=cond, title="", xlab="", ylab="") {
   if (ylab != "") {p <- p + ylab(ylab)}
   return(p)
 }
+
+summarize_correlate <- function (cond = cond) {
+  require(reshape2)
+  cat ("Summary statistics of character correlation based on ", nrow(cond), " trees.\n")
+  cat("condition\tmean\tvariance\tstddev\n")
+  list <- vector()
+  for (row in 1:ncol(cond)) {
+    names <- unlist(strsplit(colnames(cond)[row], "_"))
+    names <- paste("P(",names[1], "|", names[2], ")", sep="")
+    list <- c(list, names)
+    cat(names, ":\t", mean(cond[,row]), "\t",var(cond[,row]),"\t",sd(cond[,row]), "\n")
+  }
+  colnames(cond) <- list
+  d <- melt(cond)
+  pairwise.wilcox.test(d$value, d$variable, p.adjust = "none")
+}
+  
